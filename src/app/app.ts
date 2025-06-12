dec79cd61279f48ffc101a5cdad2de222e781c87
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2025-06-12 03:20:57 +0200

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App implements OnInit {
+  protected title = 'ng-v20-demo-app';
+
+  ngOnInit() {
+    console.log('App initialized');
+  }
+}

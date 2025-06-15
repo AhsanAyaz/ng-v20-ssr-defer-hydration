1e1adb4d364d2e565f6aed273561e0b5597de0a6
Muhammad Ahsan Ayaz <9844254+AhsanAyaz@users.noreply.github.com>
2025-06-16 01:27:03 +0200

    initial state for defer-start

--- src/app/components/products-list/products-list.ts
+++ src/app/components/products-list/products-list.ts
@@ -1,21 +1,31 @@
-import { afterNextRender, Component, input } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { Product } from '../../interfaces';
 
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.html',
   styleUrl: './products-list.css',
+  imports: [],
+  standalone: true,
 })
 export class ProductsList {
   products = input.required<Product[]>();
+  loading = signal(true);
 
   constructor() {
-    afterNextRender(() => {
-      console.log(
-        'ProductsList component initialized with products:',
-        this.products()
-      );
-    });
+    this.heavyComputation();
+  }
+
+  private heavyComputation() {
+    console.log('Starting heavy computation...');
+    const startTime = performance.now();
+    let result = 0;
+    for (let i = 0; i < 2000000000; i++) {
+      result += Math.sqrt(i);
+    }
+    const endTime = performance.now();
+    console.log(`Heavy computation finished in ${endTime - startTime} ms`);
+    this.loading.set(false);
   }
 
   addToCart(product: Product) {
